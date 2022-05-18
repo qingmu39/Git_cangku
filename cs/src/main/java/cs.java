cffd81dd7811c66ea5650430e5553af937de2d76
qingmu <1275668982@qq.com>
2022-05-18 23:41:50 +0800

    1

--- cs/src/main/java/cs.java
+++ cs/src/main/java/cs.java
@@ -1,5 +1,7 @@
 public class cs {
     public static void main(String[] args) {
         System.out.println("xxxxx");
+        System.out.println("xxxxx");
+        System.out.println("xxxxx");
     }
 }
